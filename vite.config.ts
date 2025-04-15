87f817d223d42b011209a082e3651f6cb3dc1b40
Zuaki21 <specialkazoo21@gmail.com>
2025-04-15 13:25:57 +0900

    変更

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/shinrichan/',  // ← ここにリポジトリ名を指定
+  base: process.env.NODE_ENV === 'production' ? '/shinrichan/' : '/',
   plugins: [react()],
 })
